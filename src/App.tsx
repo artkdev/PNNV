57553680b62e4706afa1b5da4246b0cd1eeae85e
Kate <kateiskra99@gmail.com>
2023-01-23 17:05:17 +0200

    adding all geko data about coins

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react"
+import ReactJson from "react-json-view"
 import "./App.css"
 
 function App() {
   const [exchangeData, setExchangeData] = useState<any>()
-  const [priceChangeDataDay, setPriceChangeDataDay] = useState<any>()
-  const [priceChangeDataHour, setPriceChangeDataHour] = useState<any>()
-  const [priceChangeDataWeek, setPriceChangeDataWeek] = useState<any>()
-  const [krakenAssetPairs, setKrakenAssetPairs] = useState<any>()
-  const [krakenTickerInformation, setKrakenTickerInformation] = useState<any>()
-  const [krakenOHLCData, setKrakenOHLCData] = useState<any>()
+  const [coinsGeko, setCoinsGeko] = useState<any[]>()
+  const [market, setMarket] = useState<any>()
+  const [coinDetails, setCoinDetails] = useState<any>()
+  const [tickers, setTickers] = useState<any>()
+  const [historyData, setHistoryData] = useState<any>()
+  const [marketChart, setMarketChart] = useState<any>()
+  const [marketRange, setMarketRange] = useState<any>()
+  const [ohlc, setOhlc] = useState<any>()
   const [currentCoin, setCurrentCoin] = useState<string>("BNB")
   const [currentCurrency, setCurrentCurrency] = useState<string>("USDT")
 
@@ -17,14 +20,23 @@ function App() {
 
   useEffect(() => {
     fetchExchangePrice(currentCoin, currentCurrency)
-    fetchPriceChangeDay(currentCoin, currentCurrency)
-    fetchPriceChangeHour(currentCoin, currentCurrency)
-    fetchPriceChangeWeek(currentCoin, currentCurrency)
-    fetchKrakenAssetPairs()
-    fetchKrakenTickerInformation()
-    fetchKrakenOHLCData()
+    fetchCoins()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    if (coinsGeko) {
+      filterCoin(currentCoin)
+      fetchMarket(currentCoin, currentCurrency)
+      fetchCoinDetails(currentCoin)
+      fetchTickers(currentCoin)
+      fetchHistoryData(currentCoin)
+      fetchMarketChart(currentCoin, currentCurrency)
+      fetchMarketRange(currentCoin, currentCurrency)
+      fetchOHLC(currentCoin, currentCurrency)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [coinsGeko])
 
   const fetchExchangePrice = async (
     _currentCoin: string,
@@ -41,82 +53,145 @@ function App() {
     }
   }
 
-  const fetchPriceChangeDay = async (
-    _currentCoin: string,
-    _currentCurrency: string
+  const fetchCoins = async () => {
+    try {
+      const data = await fetch(`https://api.coingecko.com/api/v3/coins/list`)
+      const json = await data.json()
+      setCoinsGeko(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const filterCoin = (symbol?: string) => {
+    const coin = coinsGeko?.find((item) => {
+      return item.symbol === symbol?.toLowerCase()
+    })
+    return coin?.id
+  }
+
+  const fetchMarket = async (
+    _currentCoin?: string,
+    _currentCurrency?: string
   ) => {
     try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      let cur
+      if (_currentCurrency === "USDT") {
+        cur = _currentCurrency.slice(0, -1)
+      } else {
+        cur = _currentCurrency
+      }
       const data = await fetch(
-        `https://api.binance.com/api/v3/ticker?symbol=${_currentCoin}${_currentCurrency}&windowSize=1d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${cur}&ids=${currentId}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
       )
       const json = await data.json()
-      setPriceChangeDataDay(json)
+      setMarket(json[0])
     } catch (err) {
       console.error(err)
     }
   }
 
-  const fetchPriceChangeHour = async (
-    _currentCoin: string,
-    _currentCurrency: string
+  const fetchCoinDetails = async (_currentCoin?: string) => {
+    try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${currentId}`
+      )
+      const json = await data.json()
+      setCoinDetails(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const fetchTickers = async (_currentCoin?: string) => {
+    try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${currentId}`
+      )
+      const json = await data.json()
+      setTickers(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const fetchHistoryData = async (_currentCoin?: string) => {
+    try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${currentId}/history?date=30-12-2017`
+      )
+      const json = await data.json()
+      setHistoryData(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const fetchMarketChart = async (
+    _currentCoin?: string,
+    _currentCurrency?: string
   ) => {
     try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      let cur
+      if (_currentCurrency === "USDT") {
+        cur = _currentCurrency.slice(0, -1)
+      } else {
+        cur = _currentCurrency
+      }
       const data = await fetch(
-        `https://api.binance.com/api/v3/ticker?symbol=${_currentCoin}${_currentCurrency}&windowSize=1h`
+        `https://api.coingecko.com/api/v3/coins/${currentId}/market_chart?vs_currency=${cur}&days=1`
       )
       const json = await data.json()
-      setPriceChangeDataHour(json)
+      setMarketChart(json)
     } catch (err) {
       console.error(err)
     }
   }
 
-  const fetchPriceChangeWeek = async (
-    _currentCoin: string,
-    _currentCurrency: string
+  const fetchMarketRange = async (
+    _currentCoin?: string,
+    _currentCurrency?: string
   ) => {
     try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      let cur
+      if (_currentCurrency === "USDT") {
+        cur = _currentCurrency.slice(0, -1)
+      } else {
+        cur = _currentCurrency
+      }
       const data = await fetch(
-        `https://api.binance.com/api/v3/ticker?symbol=${_currentCoin}${_currentCurrency}&windowSize=7d`
+        `https://api.coingecko.com/api/v3/coins/${currentId}/market_chart/range?vs_currency=${cur}&from=1392577232&to=1422577232`
       )
       const json = await data.json()
-      setPriceChangeDataWeek(json)
+      setMarketRange(json)
     } catch (err) {
       console.error(err)
     }
   }
 
-  const fetchKrakenAssetPairs = async () => {
+  const fetchOHLC = async (
+    _currentCoin?: string,
+    _currentCurrency?: string
+  ) => {
     try {
+      const currentId = filterCoin(_currentCoin ? _currentCoin : currentCoin)
+      let cur
+      if (_currentCurrency === "USDT") {
+        cur = _currentCurrency.slice(0, -1)
+      } else {
+        cur = _currentCurrency
+      }
       const data = await fetch(
-        "https://api.kraken.com/0/public/AssetPairs?pair=XXBTZUSD,XETHXXBT"
+        `https://api.coingecko.com/api/v3/coins/${currentId}/ohlc?vs_currency=${cur}&days=1`
       )
       const json = await data.json()
-      setKrakenAssetPairs(json?.result)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  const fetchKrakenTickerInformation = async () => {
-    try {
-      const data = await fetch(
-        "https://api.kraken.com/0/public/Ticker?pair=XBTUSD"
-      )
-      const json = await data.json()
-      setKrakenTickerInformation(json?.result)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  const fetchKrakenOHLCData = async () => {
-    try {
-      const data = await fetch(
-        "https://api.kraken.com/0/public/OHLC?pair=XBTUSD"
-      )
-      const json = await data.json()
-      setKrakenOHLCData(json?.result)
+      setOhlc(json)
     } catch (err) {
       console.error(err)
     }
@@ -124,72 +199,73 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-body">
+      <div className="App-body">
         <h1>
           (Binance) Price for{" "}
           <div>
             <select
               onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
                 fetchExchangePrice(e.target.value, currentCurrency)
-                fetchPriceChangeDay(e.target.value, currentCurrency)
-                fetchPriceChangeHour(e.target.value, currentCurrency)
-                fetchPriceChangeWeek(e.target.value, currentCurrency)
+                fetchMarket(e.target.value, currentCurrency)
+                fetchCoinDetails(e.target.value)
+                fetchTickers(e.target.value)
+                fetchHistoryData(e.target.value)
+                fetchMarketChart(e.target.value, currentCurrency)
                 setCurrentCoin(e.target.value)
               }}
             >
               {coins.map((coin) => (
                 <option key={coin}>{coin}</option>
               ))}
-            </select>{" "}
+            </select>
           </div>
-          to{" "}
+          to
           <select
             onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
               fetchExchangePrice(currentCoin, e.target.value)
-              fetchPriceChangeDay(currentCoin, e.target.value)
-              fetchPriceChangeHour(currentCoin, e.target.value)
-              fetchPriceChangeWeek(currentCoin, e.target.value)
+              fetchMarket(currentCoin, e.target.value)
+              fetchMarketChart(currentCurrency, e.target.value)
               setCurrentCurrency(e.target.value)
             }}
           >
             {currency.map((cur) => (
               <option key={cur}>{cur}</option>
             ))}
-          </select>{" "}
-          : {parseFloat(exchangeData?.price).toFixed(2)}
+          </select>
         </h1>
-        <p>
-          Info {currentCoin} price change (1h):{" "}
-          {parseFloat(priceChangeDataHour?.priceChangePercent).toFixed(2)}%
-        </p>
-        <p>
-          Info {currentCoin} price change (24h):{" "}
-          {parseFloat(priceChangeDataDay?.priceChangePercent).toFixed(2)}%
-        </p>
-        <p>
-          Info {currentCoin} price change (7d):{" "}
-          {parseFloat(priceChangeDataWeek?.priceChangePercent).toFixed(2)}%
-        </p>
-        <h1>Kraken requests</h1>
-        <div>
-          1 Tradable asset pairs wsname: {krakenAssetPairs?.XETHXXBT?.wsname}{" "}
-          (XETHXXBT) costmin: {krakenAssetPairs?.XETHXXBT?.costmin}
+        <div className="Accordion-container">
+          exchange data (Binance)
+          <ReactJson src={exchangeData} theme="bright:inverted" collapsed />
         </div>
-        <div>
-          2 Tradable asset pairs wsname: {krakenAssetPairs?.XXBTZUSD?.wsname}{" "}
-          (XXBTZUSD) costmin: {krakenAssetPairs?.XXBTZUSD?.costmin}
+        <div className="Accordion-container">
+          /coins/markets markets info (CoinGeko)
+          <ReactJson src={market} theme="bright:inverted" collapsed />
         </div>
-        <div>
-          Kraken ticker information(XBTUSD):{" "}
-          {krakenTickerInformation?.XXBTZUSD?.a[0]},{" "}
-          {krakenTickerInformation?.XXBTZUSD?.a[1]},{" "}
-          {krakenTickerInformation?.XXBTZUSD?.a[2]}
+        <div className="Accordion-container">
+          /coins/(id) details (CoinGeko)
+          <ReactJson src={coinDetails} theme="bright:inverted" collapsed />
         </div>
-        <div>
-          Kraken OHLC Data(XXBTZUSD) (same as candlestick?) last:{" "}
-          {krakenOHLCData?.last}
+        <div className="Accordion-container">
+          /coins/(id)/tickers (CoinGeko)
+          <ReactJson src={tickers} theme="bright:inverted" collapsed />
         </div>
-      </header>
+        <div className="Accordion-container">
+          /coins/(id)/history (CoinGeko)
+          <ReactJson src={historyData} theme="bright:inverted" collapsed />
+        </div>
+        <div className="Accordion-container">
+          /coins/(id)/market_chart (CoinGeko)
+          <ReactJson src={marketChart} theme="bright:inverted" collapsed />
+        </div>
+        <div className="Accordion-container">
+          /coins/(id)/market_chart/range (CoinGeko)
+          <ReactJson src={marketRange} theme="bright:inverted" collapsed />
+        </div>
+        <div className="Accordion-container">
+          /coins/(id)/ohlc (CoinGeko)
+          <ReactJson src={ohlc} theme="bright:inverted" collapsed />
+        </div>
+      </div>
     </div>
   )
 }
