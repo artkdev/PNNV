fca45629573084bab299588d9c43bfd67d7f3665
Kate <kateiskra99@gmail.com>
2023-03-10 12:07:17 +0200

    hover + focus of inputs, remove unused

--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -15,36 +15,31 @@ export const SInputArea = styled.div`
 `
 
 export const StyledInput = styled.input`
-  font-size: 1.3em;
   padding: 0.5em;
   border-radius: 6px 0px 0px 6px;
   box-shadow: none;
   border: 1.5px solid rgb(204, 204, 204);
+  outline: none;
+  font-size: 1.3em;
+  height: 100px;
 
   ::-webkit-inner-spin-button,
   ::-webkit-outer-spin-button {
     -webkit-appearance: none;
     margin: 0;
   }
-`
 
-export const StyledSelect = styled.select`
-  width: 150px;
-  min-width: 150px;
-  height: 75px;
-  padding: 0px 14px;
-  border-radius: 0px 6px 6px 0px;
-
-  box-shadow: none;
-  background-color: rgb(244, 246, 248);
-  border: 1.5px solid rgb(204, 204, 204);
-  font-size: 48px;
-  font-weight: 300;
-  text-align: center;
+  &:focus,
+  &:hover {
+    border: 2.5px solid #2684ff;
+  }
 `
 
 export const ReactSelectElement = styled(Select)`
-  .react-select__value-container {
+  .react-select__control {
     width: 200px;
+    height: 100px;
+
+    border-radius: 0px 6px 6px 0px;
   }
 `
